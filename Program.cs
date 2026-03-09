bf2071a0675666bd6b60c06c939095e1b3541dad
Kondratenko Mikhailo <mike.kondr@gmail.com>
2026-03-09 23:39:36 +0200

    finish

--- Program.cs
+++ Program.cs
@@ -1,10 +1,62 @@
-﻿namespace LR2
+namespace LR2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount(1000);
+            AccountMonitor monitor = new AccountMonitor(account);
+
+            Console.WriteLine("Створено банківський рахунок з початковим балансом 1000.");
+
+            account.Deposit(500);
+            account.Withdraw(200);
+            account.Withdraw(1500);
+        }
+    }
+
+    public class BankAccount
+    {
+        private int _balance;
+
+        public delegate void BalanceChangedHandler(string message);
+        public event BalanceChangedHandler? BalanceChangedDeposit;
+        public event BalanceChangedHandler? BalanceChangedWithdraw;
+
+        public BankAccount(int initialBalance)
+        {
+            _balance = initialBalance;
+        }
+
+        public void Deposit(int amount)
+        {
+            _balance += amount;
+            BalanceChangedDeposit?.Invoke($"Депозит на суму {amount}. Поточний баланс: {_balance}");
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount > _balance)
+            {
+                Console.WriteLine("Недостатньо коштів!");
+                return;
+            }
+            _balance -= amount;
+            BalanceChangedWithdraw?.Invoke($"Зняття на сумму {amount}. Поточний баланс: {_balance}");
+        }
+    }
+
+    public class  AccountMonitor
+    {
+        public AccountMonitor(BankAccount account)
+        {
+            account.BalanceChangedDeposit += OnBalanceChanged;
+            account.BalanceChangedWithdraw += OnBalanceChanged;
+        }
+
+        public void OnBalanceChanged(string message)
+        {
+            Console.WriteLine($"{message}");
         }
     }
 }
